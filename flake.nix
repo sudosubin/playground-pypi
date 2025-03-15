02f96f2915db050abbc9f3255b6ea27805893fad
sudosubin <sudosubin@gmail.com>
2025-03-15 11:18:23 +0900

    feat: add nix flake files

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "stockmind";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs platforms;
+      forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs { inherit system; }));
+
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            python313Packages.build
+            python313Packages.twine
+          ];
+        };
+      });
+    };
+}
